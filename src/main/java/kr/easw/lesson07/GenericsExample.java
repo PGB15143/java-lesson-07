ade3bc270f77979dc96794ad16152fe121c7c623
PGB15143 <144985256+PGB15143@users.noreply.github.com>
2024-05-09 13:31:46 +0900

    Update GenericsExample.java

--- src/main/java/kr/easw/lesson07/GenericsExample.java
+++ src/main/java/kr/easw/lesson07/GenericsExample.java
@@ -1,6 +1,5 @@
 package kr.easw.lesson07;
 
-
 /**
  * 해당 클래스는 익명 클래스 및 제너릭스에 대해 조금 더 친숙해지기 위해 구성된 문제입니다.
  *
@@ -10,12 +9,12 @@ package kr.easw.lesson07;
 public class GenericsExample {
 
     public static void main(String[] args) {
-        Recipe<Ingredient, Cake> recipe = new Recipe<>(new RottenEgg()) {
+        Recipe<Ingredient, Cake> recipe = new Recipe<>(new Egg()) {
             @Override
             public Cake cook() {
                 // 제너릭스를 통해 클래스를 확정했음으로, T 제너릭스가 Ingredient를 상속받는것이 보장됩니다.
                 // 그러므로, 원래라면 호출할 수 없는 Ingredient의 mix() 메서드를 호출할 수 있게 됩니다.
-                getIngredient().mix();
+            	getIngredient().mix();
                 return new Cake();
             }
         };
